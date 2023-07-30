bf54e1712a6042c4149e43a0c364e36a4d240cc8
Miguel Mota <migueloureiromota@gmail.com>
2023-07-30 15:23:06 +0100

    Update ft_strcapitalize.c
    Added some comments for better understanding of the code

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -16,7 +16,7 @@
 
 int main()
 {
-	char c[] = "salut, Comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+	char c[] = "salut, Comment tu vas ? 42mots quarANte-deux; cinquante+et+un";
 	printf("%s\n\n", c);
 	ft_strcapitalize(c);
 	printf("%s\n\n", c);
@@ -28,23 +28,23 @@ char	*ft_strcapitalize(char *str)
 	int	check;
 	int	tick;
 
-	tick = 0;
+	tick = 0; //if tick = 0 then it's outside of a word and if tick = 1 it means it's already in the word and the first letter was capitalized
 	i = 0;
 	while (str[i] != '\0')
 	{
 		check = str[i];
-		if (check >= 97 && check <= 122 && tick == 0)
+		if (check >= 97 && check <= 122 && tick == 0) //check if it's not in a word and it's a lowercase letter
 		{
 			str[i] = check - 32;
 			tick = 1;
 		}
-		if (check >= 65 && check <= 90 && tick == 1)
+		if (check >= 65 && check <= 90 && tick == 1) //check if there is a capitalized letter in the word
 			str[i] = check + 32;
-		if ((check < 97 && check > 90) || (check > 122) || (check < 65)) 
+		if ((check < 97 && check > 90) || (check > 122) || (check < 65)) //check if the word has ended 
 			tick = 0;
-		if (check >= 65 && check <= 90 && tick == 0)
+		if (check >= 65 && check <= 90 && tick == 0) //check if a word starts with a capitalized letter already
 			tick = 1;
-		if (check >= 48 && check <= 57)
+		if (check >= 48 && check <= 57) //check if a word starts with numbers
 			tick = 1;
 		i++;
 	}
